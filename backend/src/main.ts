2105dcfe5f93ff2f077fdf53dc766636db365806
Luis Roberto <luisrobertopenav@gmail.com>
2024-05-31 11:26:24 -0700

    white list request params

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 }
 bootstrap();
