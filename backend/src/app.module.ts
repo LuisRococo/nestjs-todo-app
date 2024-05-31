98dec70edb8af197f01ebcff0b7b86ad6dc1ac6c
Luis Roberto <luisrobertopenav@gmail.com>
2024-05-31 14:39:55 -0700

    refactor env backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,28 +3,38 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { Task } from './tasks/task.identity';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number.parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [User, Task],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.getOrThrow('DB_HOST'),
+          port: configService.getOrThrow('DB_PORT'),
+          username: configService.getOrThrow('DB_USER'),
+          password: configService.getOrThrow('DB_PASSWORD'),
+          database: configService.getOrThrow('DB_NAME'),
+          entities: [User, Task],
+          synchronize: true,
+        };
+      },
     }),
     JwtModule.register({
       secretOrPrivateKey: process.env.SECRET,
       global: true,
       signOptions: { expiresIn: '7d' },
+    }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
     }),
     UsersModule,
     AuthModule,
